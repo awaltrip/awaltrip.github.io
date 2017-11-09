1fc54c114baaae6dc361464910c5336334eea782
Anna Waltrip <annawaltrip@gmail.com>
2017-11-08 23:52:19 -0500

    added jquery file, icon, classes for dark theme

---
+++ jquery-dark.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $(".fa-moon-o").on("click", function(event) {
+        //dark theme
+    });
+
+    $(".fa-sun-o").on("click", function(event) {
+        //default theme
+    });
+    
+});
